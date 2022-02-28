dfd61730c632b7711233c2e7716cede74be97ff2
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-28 14:02:01 +0100

    Created a login (for SBoot only for now)

---
+++ fitapp-backend/src/main/java/com/project/Springbootbackend/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.project.Springbootbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "you don't need to be logged in";
+    }
+
+    @GetMapping("/not-restricted")
+    public String notRestricted() {
+        return "you don't need to be logged in";
+    }
+
+    @GetMapping("/restricted")
+    public String restricted() {
+        return "if you see this you are logged in";
+    }
+}
